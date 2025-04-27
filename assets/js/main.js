5cb762f8769d47cab60074a554c98b849e2cff96
Tony Lea <tnylea@gmail.com>
2025-04-27 13:56:09 -0400

    Adding latest updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 // Add your javascript here
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
+import intersect from '@alpinejs/intersect'
 
 Alpine.plugin(collapse);
+Alpine.plugin(intersect);
 
 Alpine.store('theme', {
     init() {
